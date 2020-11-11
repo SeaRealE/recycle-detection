10061de6120cb00487cf6d82c515dcc6e8c27831
HaeJin Lee <seareale@gmail.com>
2020-11-12 06:15:45 +0900

    optimal threshold value : 0.98 -> 0.49

--- predict.py
+++ predict.py
@@ -59,7 +59,7 @@ def parse_args():
     parser.add_argument(
         '--show-score-thr',
         type=float,
-        default=0.98,
+        default=0.49,
         help='score threshold (default: 0.3)')
     parser.add_argument(
         '--gpu-collect',
