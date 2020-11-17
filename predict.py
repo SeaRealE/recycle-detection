2d4c18b4d6aca829923dbdb5b96b86ec5ef22a74
HaeJin Lee <seareale@gmail.com>
2020-11-17 13:32:11 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -175,11 +175,11 @@ def main():
     size = len(glob(filepath + '/*.jpg'))
     check = [False for i in range(size)]
     filename_list = [name[len(filepath)+1:] for name in images_list]
-    dic = OrderedDict({key:{'image_id': key, 'file_name': filename_list[key], 'object':[{'box':[], 'label': ""}]} for key in range(size)})
+    dic = OrderedDict({key:{'id': key, 'file_name': filename_list[key], 'object':[{'box':[], 'label': ""}]} for key in range(size)})
     
     f = open('t3_res_0030.json', 'w')
     for item in json_data:
-        cur_id = item["image_id"] - 1
+        cur_id = item["id"] - 1
 
         if check[cur_id] == False: # nothing 
             x, y, w, h = [int(i) for i in item["bbox"]]
